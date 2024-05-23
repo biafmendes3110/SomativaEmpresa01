897aed10e3ecdf6114105ea59903ff81fcc96654
biafmendes3110 <132359735+biafmendes3110@users.noreply.github.com>
2024-05-23 14:35:51 -0300

    Add files via upload

---
+++ SomativaEmpresa/src/main/java/com/somativaempresa/SomativaEmpresaApplication.java
@@ -0,0 +1,13 @@
+package com.somativaempresa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomativaEmpresaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomativaEmpresaApplication.class, args);
+	}
+
+}
---
+++ SomativaEmpresa/src/test/java/com/somativaempresa/SomativaEmpresaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.somativaempresa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomativaEmpresaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
